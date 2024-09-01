8c5bbc4782b7e3e34083c881b4fddd433a5dd4bb
Mohit Aasirwal <mohitaasirwal@gmail.com>
2024-09-01 21:39:06 +0530

    feat: commitlint added

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "wip", "build", "refactor"]],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-max-length": [2, "always", 8],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
+    "subject-max-length": [2, "always", 60],
+  },
+};
