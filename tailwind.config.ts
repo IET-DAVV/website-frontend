d4c9f0c5f57b0a26058de2b8306ab3c61620e06a
Mohit Aasirwal <mohitaasirwal@gmail.com>
2024-09-03 23:15:31 +0530

    Revert "Ashutosh Dwivedi : Header and Footer component done"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{html,js}"
   ],
   theme: {
     extend: {
