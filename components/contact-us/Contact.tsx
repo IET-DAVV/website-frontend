67aac847cb1b6192734ea8af204a6e7a8f875a1e
Mohit Aasirwal <mohitaasirwal@gmail.com>
2024-09-17 18:26:32 +0530

    feat: contact section

--- components/contact-us/Contact.tsx
+++ components/contact-us/Contact.tsx
@@ -14,35 +14,34 @@ const manrope = Manrope({
 
 const ContactUs = () => {
   return (
-    <div className="container mx-auto mt-20 px-4 py-16">
-      {/* Contact Us Heading */}
-      <h1
-        className={`${playfair.className} text-4xl font-semibold text-center text-light-blue`}
-      >
-        Contact us
-      </h1>
-      <p className="text-center text-light-blue mt-2">
-        Do you have any questions? Don’t hesitate to contact us
-      </p>
-
+    <div className="flex flex-col space-y-10 mx-auto mt-36 p-16">
+      <div className="flex flex-col space-y-2">
+        <h1
+          className={`${playfair.className} text-6xl font-semibold text-center text-light-blue`}
+        >
+          Contact us
+        </h1>
+        <p className="text-center text-light-blue mt-2">
+          Do you have any questions? Don&apos;t hesitate to contact us
+        </p>
+      </div>
       <div
-        className={`${playfair.className} grid grid-cols-1 md:grid-cols-2 gap-8 mt-12`}
+        className={`${playfair.className} grid grid-cols-1 md:grid-cols-2 gap-20 h-full `}
       >
-        {/* Get in Touch Section */}
         <div className="space-y-6">
           <div>
-            <h2 className={`${playfair.className} text-4xl`}>Get in Touch</h2>
+            <h2 className={`${playfair.className} text-4xl text-black`}>
+              Get in Touch
+            </h2>
           </div>
 
           {/* Address Section */}
-          <div className="text-gray-700">
+          <div className="text-black flex flex-col space-y-2">
             <div className="flex items-center text-light-blue">
               <FaMapMarkerAlt className="mr-2" size={20} />
-              <h3 className={`${manrope.className} text-l font-medium`}>
-                Address
-              </h3>
+              <h3 className={`${manrope.className} font-medium`}>Address</h3>
             </div>
-            <p className={`${manrope.className} ml-8 text-gray-800 mt-2`}>
+            <p className={`${manrope.className}  text-black`}>
               Institute of Engineering and Technology, Devi Ahilya
               Vishwavidyalaya, <br />
               Khandwa Road Indore-452017 (M.P)
@@ -50,29 +49,23 @@ const ContactUs = () => {
           </div>
 
           {/* Email Section */}
-          <div className="text-gray-700">
+          <div className="text-black flex flex-col space-y-2">
             <div className="flex items-center text-light-blue">
               <FaEnvelope className="mr-2" size={20} />
-              <h3 className={`${manrope.className} text-l font-medium`}>
-                Email
-              </h3>
+              <h3 className={`${manrope.className} font-medium`}>Email</h3>
             </div>
-            <p className={`${manrope.className} ml-8 text-gray-800 mt-2`}>
+            <p className={`${manrope.className} text-black `}>
               contactiet@ietdavv.edu.in
             </p>
           </div>
 
           {/* Phone Section */}
-          <div className="text-gray-700">
+          <div className="text-black flex flex-col space-y-2">
             <div className="flex items-center text-light-blue">
               <FaPhoneAlt className="mr-2" size={20} />
-              <h3 className={`${manrope.className} text-l font-medium`}>
-                Call Us
-              </h3>
+              <h3 className={`${manrope.className} font-medium`}>Call Us</h3>
             </div>
-            <p className={`${manrope.className} ml-8 text-gray-800 mt-2`}>
-              0731-2352678
-            </p>
+            <p className={`${manrope.className} text-black`}>0731-2352678</p>
           </div>
 
           {/* Map Section */}
@@ -89,36 +82,36 @@ const ContactUs = () => {
         </div>
 
         {/* Send Message Section */}
-        <div className="space-y-6">
-          <h2 className={`${playfair.className} text-4xl`}>
+        <div className="space-y-6 h-full">
+          <h2 className={`${playfair.className} text-4xl text-black`}>
             Send us a message
           </h2>
-          <form className="space-y-4">
+          <form className={`${manrope.className} space-y-7`}>
             <div>
               <input
                 type="text"
                 placeholder="Full Name"
-                className="w-full border border-gray-300 p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-600"
+                className="w-full border text-black text-sans border-black p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-600"
               />
             </div>
             <div>
               <input
                 type="email"
                 placeholder="Email"
-                className="w-full border border-gray-300 p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-600"
+                className="w-full border text-black text-sans border-black p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-600"
               />
             </div>
             <div>
               <textarea
                 placeholder="Type your message here"
                 rows={13}
-                className="w-full border border-gray-300 p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-600"
+                className="w-full border text-black text-sans border-black p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-600"
               ></textarea>
             </div>
             <div>
               <button
                 type="submit"
-                className="bg-dark-blue text-white text-center w-full text-xl px-32 py-6 rounded-md hover:bg-sky-700"
+                className="bg-dark-blue text-white text-center w-full text-xl py-2 hover:bg-light-blue transition duration-300 ease-linear rounded-md hover:bg-sky-700"
               >
                 Submit
               </button>
