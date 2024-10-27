83646827f63ca728228c494a9f2f51b4c941a0d1
faroutfarhaan <133531252+faroutfarhaan@users.noreply.github.com>
2024-10-27 20:21:23 +0530

    feat: landmarks component added

--- app/temp/page.tsx
+++ app/temp/page.tsx
@@ -9,6 +9,7 @@ import Director from "@/components/about-page/Director";
 import VC from "@/components/about-page/VC";
 import Mission from "@/components/about-page/Mission";
 import Vision from "@/components/about-page/Vision";
+import Landmarks from "@/components/about-page/Landmarks";
 const playfair = Playfair_Display({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -19,6 +20,7 @@ const page = () => {
       <Navbar />
       <About />
       <History />
+      <Landmarks />
       <Vision />
       <Mission />
       <Director />
