5774f4cdb2551067b0967e4a83893d77f54ab03b
Moh Farhan Patel <133531252+faroutfarhaan@users.noreply.github.com>
2025-06-25 18:32:50 +0530

    Add Vision and Mission Components for IET and DAVV (#23)
    * feat: vision component added
    
    * feat: mission component added
    
    * feat: landmarks component added

---
+++ constants/about-page/landmarks.ts
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import { Achievement } from "grommet-icons";
+export const landmarks = [
+  {
+    id: "1",
+    // icon: () => <Achievement size="large" color="brand" />,
+    heading: "Lorem Ipsum",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. aEtiam eu turpis molestie",
+  },
+  {
+    id: "2",
+    // icon:  <Achievement size='large' color='brand'/>,
+    heading: "Lorem Ipsum",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. aEtiam eu turpis molestie",
+  },
+  {
+    id: "3",
+    // icon: <Achievement size='large' color='brand'/> ,
+    heading: "Lorem Ipsum",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. aEtiam eu turpis molestie",
+  },
+  {
+    id: "4",
+    // icon: <Achievement size='large' color='brand'/> ,
+    heading: "Lorem Ipsum",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. aEtiam eu turpis molestie",
+  },
+  {
+    id: "5",
+    // icon: <Achievement size='large' color='brand'/> ,
+    heading: "Lorem Ipsum",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. aEtiam eu turpis molestie",
+  },
+  {
+    id: "6",
+    // icon: <Achievement size='large' color='brand'/> ,
+    heading: "Lorem Ipsum",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. aEtiam eu turpis molestie",
+  },
+  {
+    id: "7",
+    // icon: <Achievement size='large' color='brand'/> ,
+    heading: "Lorem Ipsum",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. aEtiam eu turpis molestie",
+  },
+  {
+    id: "8",
+    // icon: <Achievement size='large' color='brand'/>,
+    heading: "Lorem Ipsum",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. aEtiam eu turpis molestie",
+  },
+];
