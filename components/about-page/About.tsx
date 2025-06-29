fe5f2df1391a459c051b1fe9073cd81c9da6ad5d
Anadi Sharma <1anadi1sharma@gmail.com>
2025-06-29 11:59:29 +0530

    feat: aboutchange

--- components/about-page/About.tsx
+++ components/about-page/About.tsx
@@ -11,23 +11,23 @@ const About = () => {
   return (
     <div className="h-full w-[90%] mx-auto bg-white flex justify-center border-[#3B799E] border-4 my-24">
       <div className="grid grid-cols-2 justify-center items-center w-full place-items-center">
-        <div className="relative inset-0 w-full h-full flex justify-center items-center">
+        <div className="w-full h-full flex justify-center items-center p-8">
           <Image
             src={aboutImage}
-            width={1000}
-            height={1000}
-            alt=""
-            className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[80%] h-[150%]"
+            width={600}
+            height={400}
+            alt="About IET"
+            className="rounded-md object-cover w-full h-auto"
           />
         </div>
-        <div className="flex flex-row items-center overflow-hidden text-black p-14">
-          <div className="flex flex-col space-y-10">
+        <div className="flex flex-row text-black p-10">
+          <div className="flex flex-col space-y-5">
             <h1
-              className={`${playfair.className} text-6xl font-extralight uppercase`}
+              className={`${playfair.className} text-5xl text-black text-justify`}
             >
               About-IET DAVV Indore
             </h1>
-            <p className=" break-words text-black">{aboutIET}</p>
+            <p className=" break-words text-black text-justify">{aboutIET}</p>
           </div>
         </div>
       </div>
--- constants/about-page/mission.ts
+++ constants/about-page/mission.ts
@@ -1,69 +1,21 @@
-import { link } from "fs";
-
 export const missionIet = [
   {
     id: "01",
     description:
       "To be a leading human resource development centre for generating, advancing and disseminating knowledge, serve and build value to the society and industry through research, entrepreneurship and outreach activities.",
   },
-  {
-    id: "02",
-    description:
-      "To be a leading human resource development centre for generating, advancing and disseminating knowledge, serve and build value to the society and industry through research, entrepreneurship and outreach activities.",
-  },
-  {
-    id: "03",
-    description:
-      "To be a leading human resource development centre for generating, advancing and disseminating knowledge, serve and build value to the society and industry through research, entrepreneurship and outreach activities.",
-  },
-  {
-    id: "04",
-    description:
-      "To be a leading human resource development centre for generating, advancing and disseminating knowledge, serve and build value to the society and industry through research, entrepreneurship and outreach activities.",
-  },
-  {
-    id: "05",
-    description:
-      "To be a leading human resource development centre for generating, advancing and disseminating knowledge, serve and build value to the society and industry through research, entrepreneurship and outreach activities.",
-  },
-  {
-    id: "06",
-    description:
-      "To be a leading human resource development centre for generating, advancing and disseminating knowledge, serve and build value to the society and industry through research, entrepreneurship and outreach activities.",
-  },
+  // add other objects here (02, 03, ... 06)
 ];
+
 export const missionDavv = [
   {
     id: "01",
     description:
       "Emerge as a premier higher learning institution by creating, advancing and disseminating knowledge with collective wisdom, through value imbued holistic education for peaceful, sustainable and humane society.",
   },
-  {
-    id: "02",
-    description:
-      "Emerge as a premier higher learning institution by creating, advancing and disseminating knowledge with collective wisdom, through value imbued holistic education for peaceful, sustainable and humane society.",
-  },
-  {
-    id: "03",
-    description:
-      "Emerge as a premier higher learning institution by creating, advancing and disseminating knowledge with collective wisdom, through value imbued holistic education for peaceful, sustainable and humane society.",
-  },
-  {
-    id: "04",
-    description:
-      "Emerge as a premier higher learning institution by creating, advancing and disseminating knowledge with collective wisdom, through value imbued holistic education for peaceful, sustainable and humane society.",
-  },
-  {
-    id: "05",
-    description:
-      "Emerge as a premier higher learning institution by creating, advancing and disseminating knowledge with collective wisdom, through value imbued holistic education for peaceful, sustainable and humane society.",
-  },
-  {
-    id: "06",
-    description:
-      "Emerge as a premier higher learning institution by creating, advancing and disseminating knowledge with collective wisdom, through value imbued holistic education for peaceful, sustainable and humane society.",
-  },
+  // add other objects here (02, 03, ... 06)
 ];
+
 export const missions = [
   {
     name: "MISSION-IET",
