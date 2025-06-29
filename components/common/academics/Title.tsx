57e5008e0d93fab48b88819f608ae4ae3ed11aaf
UthkarshMandloi <24bcs171@ietdavv.edu.in>
2025-06-30 03:04:09 +0530

    feat: titel error solved

--- components/common/academics/Title.tsx
+++ components/common/academics/Title.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import "@/styles/fonts.css"; // 👈 Font definitions
+import "@/styles/fonts.css"; // 👈 Import your font definitions
 import "@/app/globals.css";
 
 interface TitleProps {
   title: string;
-  className?: string; // 👈 Optional prop for custom styles
 }
 
-const Title: React.FC<TitleProps> = ({ title, className = "" }) => {
+const Title: React.FC<TitleProps> = ({ title }) => {
   return (
     <h1
-      className={`font-NewYork font-normal  text-center text-[#06779B] ${className}`}
+      className={`font-newyork text-[80px] leading-[120px] font-normal mb-4 text-center text-[#06779B]`}
     >
       {title}
     </h1>
