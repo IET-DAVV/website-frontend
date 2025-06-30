832c5b045ce630372d71bfd92231670aaf0110d5
Rudra Pratap Singh <24bcs156@ietdavv.edu.in>
2025-06-30 17:03:17 +0530

    feat: responsive aboutPage

--- components/about-page/Mission.tsx
+++ components/about-page/Mission.tsx
@@ -13,20 +13,22 @@ const Mission = () => {
 
   return (
     <>
-      {/* Tabs for mission cards */}
-      <div className="my-20 mx-16">
-        <div className="relative flex justify-center gap-40 mb-8">
-          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[18rem] h-[1px] bg-gray-300 z-0" />
+      <div className="my-10 sm:my-16 md:my-20 mx-4 sm:mx-8 md:mx-16">
+        {/* Tabs beside each other */}
+        <div className="flex justify-center items-center gap-12 sm:gap-20 mb-10">
           {missions.map((value, id) => {
             const isActive = preview === value.link;
             return (
               <button
                 key={id}
-                onClick={() => setPreview(value.link)}
-                className={`relative text-2xl transition-all duration-300 pb-2 ${
+                onClick={() => {
+                  setPreview(value.link);
+                  setActiveTab(value.name === "MISSION–IET" ? "IET" : "DAVV");
+                }}
+                className={`relative text-lg sm:text-2xl md:text-3xl transition-all duration-300 pb-2 ${
                   isActive
                     ? "text-[#3B799E] font-semibold"
-                    : "text-gray-400 font-thin"
+                    : "text-gray-400 font-light"
                 }`}
               >
                 {value.name}
@@ -34,14 +36,16 @@ const Mission = () => {
                   className={`absolute left-0 bottom-0 h-[3px] transition-all duration-300 ${
                     isActive
                       ? "w-full bg-[#3B799E]"
-                      : "w-full bg-gray-300 h-[0.7px]"
+                      : "w-full bg-gray-300 h-[0.5px]"
                   }`}
                 ></span>
               </button>
             );
           })}
         </div>
-        <div className="grid grid-cols-3 my-4">
+
+        {/* Cards Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-4">
           {preview?.map((item: any) => (
             <DesCard
               key={item.id}
