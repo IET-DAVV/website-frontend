7a0cf5cadfb91c77de65654742f21efffc6c78b7
samarJ19 <samarjoshitech@gmail.com>
2025-07-13 01:51:44 +0530

    fix: hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${manrope.className} bg-white`}>
         <HeaderContainer />
         {children}
