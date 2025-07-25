a4f2c85f3bb71cab389e75912c44cc464511e2ed
UthkarshMandloi <24bcs171@ietdavv.edu.in>
2025-07-25 21:58:42 +0530

    heading component added

--- components/achievements-carousel/Achievements.tsx
+++ components/achievements-carousel/Achievements.tsx
@@ -2,14 +2,18 @@
 import React from "react";
 import "@/styles/fonts.css";
 import Slides from "./Slides";
+import Heading from "@/components/common/academics/Heading";
 
 const Achievements = () => {
   return (
     <div className="">
       <div className="p-6 sm:p-10 grid grid-cols-1 sm:grid-cols-2 justify-center bg-white text-center sm:text-left">
-        <h1 className="text-black font-extralight text-4xl sm:text-7xl font-mono font-newyork">
-          ACHIEVEMENTS
-        </h1>
+        <div className="p-8">
+          <Heading
+            title="ACHIEVEMENTS"
+            className="text-center text-[72px]"
+          />
+        </div>
       </div>
       <div className="bg-[#3B7A9E] px-4 sm:px-8 py-6 sm:py-8">
         <Slides />
