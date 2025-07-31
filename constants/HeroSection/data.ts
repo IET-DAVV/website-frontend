707a477872f47df151e49c1039761986452fc509
kavyansh <kavyanshsaxena23@gmail.com>
2025-07-31 17:42:43 +0530

    feat: other changesacross devbranch

---
+++ constants/HeroSection/data.ts
@@ -0,0 +1,29 @@
+// constants/HeroSection/data.ts
+
+export const newsItems = [
+  "Councelling schedule session 2025-26 (for B.Tech CSBS and BDesign program)",
+  "Timetable coming soon.",
+  'Hackathon "CodeSprint 2025" registrations open till 10th July.',
+  "AI/ML Workshop on 6-7 July. Limited seats.",
+  "Internship applications open on the Placement Cell Portal.",
+  'Tech Fest "Innovatia 2025" scheduled for 20-22 August.',
+  "Convocation registration deadline: 12th July.",
+];
+
+export const announcementItems = [
+  "Mid-sem Exams start from 15th July 2025.",
+  "Timetable coming soon.",
+  'Hackathon "CodeSprint 2025" registrations open till 10th July.',
+  "AI/ML Workshop on 6-7 July. Limited seats.",
+  "Internship applications open on the Placement Cell Portal.",
+  'Tech Fest "Innovatia 2025" scheduled for 20-22 August.',
+  "Convocation registration deadline: 12th July.",
+];
+
+export const slides = [
+  "landing/mblock.png",
+  "HeroSection/IMG-20250312-WA0003.jpg",
+  "HeroSection/IMG-20250112-WA0334 (1).jpg",
+  "HeroSection/Screenshot_2025-06-29-14-52-08-27_1c337646f29875672b5a61192b9010f9.jpg",
+  "HeroSection/IMG-20250705-WA0009.jpg",
+];
