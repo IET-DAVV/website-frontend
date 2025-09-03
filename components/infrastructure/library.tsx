9042ec9edefbc805d17beff7abe919b26399cc79
Uthkarsh Mandloi <24bcs171@ietdavv.edu.in>
2025-09-03 20:35:28 +0530

    fix some image problem

--- components/infrastructure/library.tsx
+++ components/infrastructure/library.tsx
@@ -50,7 +50,7 @@ viewport={{ once: true }}
       viewport={{ once: true }}
     >
       <Image
-  src="/infrastructure/library_aerial.png"
+  src="/infrastructure/library/library_aerial.png"
         alt="IET DAVV Central Library"
         width={745}
         height={371}
