8632e301b65374674b4109bc97bdde404e7ac14e
Iamyashjain <yashjain200502@gmail.com>
2025-10-06 18:19:03 +0530

    bug again

--- app/facultyPage/page.tsx
+++ app/facultyPage/page.tsx
@@ -1,7 +1,7 @@
 "use client"; // ensure this if you're directly using hooks in this file
 
 import React, { Suspense } from "react";
-import Page from "@/components/FacultyPage/facultyData";
+import Page from "@/components/facultyPage/facultyData";
 
 const FacultyPage = () => {
   return (
